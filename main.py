dcf846356b03740b40c22793a843a6c10dff648a
Estevao Fonseca <estevaopfon@gmail.com>
2023-04-08 13:15:00 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys
+import pygame
+import click
+
+
+@click.command()
+@click.option("--fullscreen", is_flag=True, help="Pixel art will be fullscreen")
+@click.argument("files", nargs=-1)
+def main(fullscreen, files):
+	run(files, fullscreen)
+
+
+def run(files, fullscreen):
+	pygame.init()
+	width, height = 650, 750
+	window = pygame.display.set_mode((width, height))
+
+	def listen():
+		""" quit the window when user presses the red x button of the window """
+		return [pygame.quit() for event in pygame.event.get() if event.type == pygame.QUIT]
+
+	image_tv = pygame.image.load("tv.png")
+	image_tv = pygame.transform.scale(image_tv, (width, height))
+	image_sprite = []
+	for file in files:
+
+		image = pygame.image.load(file)
+		if fullscreen:
+			image = pygame.transform.scale(image, (560, 560))
+		else:
+			image = pygame.transform.scale(image, (350, 350))
+		image_sprite.append(image)
+
+	clock = pygame.time.Clock()
+	value = 0
+	while True:
+		clock.tick(4)
+		listen()
+		if value >= len(image_sprite):
+			value = 0
+
+		image = image_sprite[value]
+		x = 120
+		y = 120
+		if fullscreen:
+			x = 50
+			y = 50
+		window.blit(image, (x, y))
+		window.blit(image_tv, (0, 0))
+
+		pygame.display.update()
+
+		# Filling the window with black color
+		window.fill((0, 0, 0))
+		value += 1
+
+if __name__ == "__main__":
+	main()
